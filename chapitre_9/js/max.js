d5438232364a6fa5a1f25c6d3f1ac4525d193a0d
caiuspupus86 <31988875+caiuspupus86@users.noreply.github.com>
2017-10-11 16:31:03 +0200

    Update max.js

--- chapitre_9/js/max.js
+++ chapitre_9/js/max.js
@@ -4,11 +4,11 @@ Exercice : maximum d'un tableau
 
 var valeurs = [11, 3, 7, 2, 9, 10];
 
-var max = valeurs[0];
-// L'indice commence à 1 puisque le maximum est initialisé avec la 1ère valeur du tableau
-for (var i = 1; i < valeurs.length; i++) {
-    if (valeurs[i] > max) {
-        max = valeurs[i];
-    }
-}
+var max = -Infinity;
+// on pose la valeur max la plus faible possible
+
+valeurs.forEach(function(valeur) {
+    max = Math.max(max, valeur);
+});
+
 console.log("Le maximum des éléments vaut " + max);
